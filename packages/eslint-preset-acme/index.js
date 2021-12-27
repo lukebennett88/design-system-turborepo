ce54c3115c4fb41c84bb4a78c9125b60f299fd75
Luke Bennett <luke.bennett@brighte.com.au>
2021-12-28 10:24:03 +1100

    Add simple-import-sort ESLint plugin

--- packages/eslint-preset-acme/index.js
+++ packages/eslint-preset-acme/index.js
@@ -1,7 +1,10 @@
 module.exports = {
   extends: ["next/core-web-vitals", "prettier"],
+  plugins: ["simple-import-sort"],
   rules: {
     "react/no-unescaped-entities": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
   },
   settings: { next: { rootDir: ["./apps/*/", "./packages/*/"] } },
 };
