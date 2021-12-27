3af93cfef8a312e7934dcedbd4ccbbee136e559d
Luke Bennett <luke.bennett@brighte.com.au>
2021-12-28 10:29:43 +1100

    Add eslint-plugin-import

--- packages/eslint-preset-acme/index.js
+++ packages/eslint-preset-acme/index.js
@@ -1,10 +1,13 @@
 module.exports = {
   extends: ["next/core-web-vitals", "prettier"],
-  plugins: ["simple-import-sort"],
+  plugins: ["simple-import-sort", "import"],
   rules: {
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
     "react/no-unescaped-entities": "off",
-    "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
+    "simple-import-sort/imports": "error",
   },
   settings: { next: { rootDir: ["./apps/*/", "./packages/*/"] } },
 };
